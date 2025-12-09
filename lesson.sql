886d1b2d61d7c08aa9e0de98155829c290f06376
Your Name <you@example.com>
2025-12-09 20:00:44 +0900

    DB_Lesson対応

---
+++ lesson.sql
@@ -0,0 +1,41 @@
+INSERT INTO reports
+            (person_id,content)
+     VALUES (1, "バックエンド")
+     ;
+INSERT INTO reports
+            (person_id,content)
+     VALUES(2,"フロントエンド")
+     ;
+INSERT INTO reports
+            (person_id,content)
+     VALUES(3,"データベース")
+     ;
+INSERT INTO reports
+            (person_id,content)
+     VALUES(4,"GIT Lesson")
+     ;
+INSERT INTO reports
+            (person_id,content)
+     VALUES(5,"HTML")
+     ;
+INSERT INTO reports
+            (person_id,content)
+     VALUES(7,"CSS")
+     ;
+INSERT INTO reports
+            (person_id,content)
+     VALUES(8,"PC基礎")
+     ;
+INSERT INTO reports
+            (person_id,content)
+     VALUES(9,"DB Lesson")
+     ;
+INSERT INTO reports
+            (person_id,content)
+     VALUES(10,"IT基礎")
+     ;
+INSERT INTO reports
+            (person_id,content)
+     VALUES(11,"java Script")
+     ;
+     
